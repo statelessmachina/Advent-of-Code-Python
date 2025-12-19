2fa369e1160d7982d80855cbddde9f1173796960
Hobbes <156368504+statelessmachina@users.noreply.github.com>
2025-12-19 15:45:57 -0600

    day3/battery.py: Commenting code

--- day3/battery.py
+++ day3/battery.py
@@ -11,33 +11,36 @@ else:
 with open(sys.argv[1], 'r') as file:
     line = file.readline()
     while line:
-        line = line.strip()
-        numbers = [int(i) for i in list(line.strip())]
-        max_num = max(numbers[:-battery_len])
+        line = line.strip()                             # strips '\n'
+        numbers = [int(i) for i in list(line)]          # Turns input str of digits into int 
+        max_num = max(numbers[:-battery_len])           # finds max digit in subset of num list, 
+                                                        # beginning to battery_len from the end
         max_idx = numbers.index(max_num)
         current_batt = 0
 
-        last_digit_check = len(line)-battery_len
-        if max_idx == last_digit_check:
-            current_batt = int(line[last_digit_check:])
+    # This if/else checks index of the max_number in the list
+        last_digit_check = len(line)-battery_len        # Biggest index the max could be to return 
+                                                        #  the target battery length.
+        if max_idx == last_digit_check:                 # If the index is located less than target battery length
+            current_batt = int(line[last_digit_check:]) #  from the end, it just prints the last "battery_len" digits.
 
-        else:
-            current_batt = numbers[max_idx:]
-            min_num = 0
-            while len(current_batt) > battery_len:
+        else:                               
+            current_batt = numbers[max_idx:]            # Grabs subset of numbers starting at first max found
+            min_num = 0                                 #  then starts looking for the min digit and removes 
+            while len(current_batt) > battery_len:      #  until the len(current_batt) is equal to the target length
                 min_num = min(current_batt)
                 current_batt.remove(min_num)
 
-            temp_ans=''
+            temp_ans=''                                 # Concatenates each number together as a str then casts to int
             for i in current_batt:
                 temp_ans += str(i)
 
             current_batt = int(temp_ans)
             
-        answer += current_batt
-        #print(f'{numbers}')
+        answer += current_batt                          # Adds final battery to answer 
+        line = file.readline()                          #  and reads next line
+        #print(f'{numbers}')                            # Debug statements
         #print(f'{current_batt=} :: {answer=}')
-        line = file.readline()
 
     print(f'{answer=}')
 
