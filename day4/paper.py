f2bffad55a624d0c41efdf1beb3733dd07d08eb6
Hobbes <156368504+statelessmachina@users.noreply.github.com>
2025-12-19 15:45:57 -0600

    day4: Changes on paper.py and test_input
    Corrected test input with correct data.
    
    Added check_rolls function to paper.py. Not working correctly.
    TODO:
        - Rework code for populating rows being checked
        - Rework function to check if eligible roll

--- day4/paper.py
+++ day4/paper.py
@@ -2,21 +2,62 @@
 
 import sys
 
+def check_rolls(line_list: list[list]) -> int:
+    '''
+    Takes in matrix with 3 rows and checks the middle row for 
+      eligible rows.
+    '''
+    eligible_rolls = 0
+
+    row_len = len(line_list[1])
+    for column_i in range(row_len):
+        surround_list = list()
+        if column_i == 0:
+            # populate list clockwise 0-6 o'clock only
+            for row in range(3):
+                for column in [column_i, column_i+1]:
+                    surround_list.append(line_list[row][column])
+            rolls_around = surround_list.count('@') - 1
+
+        elif column_i == row_len-1:
+            # populate list clockwise 6-0 o'clock only
+            for row in range(3):
+                for column in [column_i-1, column_i]:
+                    surround_list.append(line_list[row][column])
+            rolls_around = surround_list.count('@') - 1
+
+        else:
+            for row in range(3):
+                for column in [column_i-1, column_i, column_i+1]:
+                    surround_list.append(line_list[row][column])
+            rolls_around = surround_list.count('@') - 1
+        
+        if rolls_around < 4:
+            eligible_rolls += 1
+
+    return eligible_rolls
+
 answer = 0 
 
 with open(sys.argv[1], 'r') as input:
-    # Grabs first 2 lines to start checking rolls
+    # Grabs first 2 line_list to start checking rolls
     # Initializes with one empty row to check top row
     line = list((input.readline()).strip())
-    lines = list()
-    lines.append(list('.' * len(line)))
+    line_list = list()
+    line_list.append(list('.' * len(line)))
 
-    while len(lines) < 3:
-        lines.append(line)
+    while len(line_list) < 2:
+        line_list.append(line)
         line = list((input.readline()).strip())
 
     while line:
-        print(f"{lines}")
-        lines = lines[1:]
-        lines.append(line)
+        line_list.append(line)
+        print(line_list)
+        answer += check_rolls(line_list)
+
+        line_list = line_list[1:]
         line = list((input.readline()).strip())
+
+    print(f"{line_list=}\n{answer=}")
+
+
