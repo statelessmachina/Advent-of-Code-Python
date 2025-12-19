26ad9af1e165d49e639735a5bf9847d5f751fdb7
Hobbes <156368504+statelessmachina@users.noreply.github.com>
2025-12-19 15:45:57 -0600

    day4/paper.py: Saving current version before reworking function
    Plan is to rework the check_rolls function to check individual roll
      as eligible true/false vs returning the total number in that row

--- day4/paper.py
+++ day4/paper.py
@@ -12,28 +12,29 @@ def check_rolls(line_list: list[list]) -> int:
     row_len = len(line_list[1])
     for column_i in range(row_len):
         surround_list = list()
-        if column_i == 0:
-            # populate list clockwise 0-6 o'clock only
-            for row in range(3):
-                for column in [column_i, column_i+1]:
-                    surround_list.append(line_list[row][column])
-            rolls_around = surround_list.count('@') - 1
+        if line_list[1][column_i] == '@':
+            if column_i == 0:
+                # populate list clockwise 0-6 o'clock only
+                for row in range(3):
+                    for column in [column_i, column_i+1]:
+                        surround_list.append(line_list[row][column])
+                rolls_around = surround_list.count('@') - 1
 
-        elif column_i == row_len-1:
-            # populate list clockwise 6-0 o'clock only
-            for row in range(3):
-                for column in [column_i-1, column_i]:
-                    surround_list.append(line_list[row][column])
-            rolls_around = surround_list.count('@') - 1
+            elif column_i == row_len-1:
+                # populate list clockwise 6-0 o'clock only
+                for row in range(3):
+                    for column in [column_i-1, column_i]:
+                        surround_list.append(line_list[row][column])
+                rolls_around = surround_list.count('@') - 1
 
-        else:
-            for row in range(3):
-                for column in [column_i-1, column_i, column_i+1]:
-                    surround_list.append(line_list[row][column])
-            rolls_around = surround_list.count('@') - 1
-        
-        if rolls_around < 4:
-            eligible_rolls += 1
+            else:
+                for row in range(3):
+                    for column in [column_i-1, column_i, column_i+1]:
+                        surround_list.append(line_list[row][column])
+                rolls_around = surround_list.count('@') - 1
+            
+            if rolls_around < 4:
+                eligible_rolls += 1
 
     return eligible_rolls
 
@@ -43,21 +44,24 @@ with open(sys.argv[1], 'r') as input:
     # Grabs first 2 line_list to start checking rolls
     # Initializes with one empty row to check top row
     line = list((input.readline()).strip())
-    line_list = list()
-    line_list.append(list('.' * len(line)))
-
-    while len(line_list) < 2:
+    buffer = list('.' * len(line))
+    line_list = [buffer]
+    for i in range(2):
         line_list.append(line)
         line = list((input.readline()).strip())
 
     while line:
-        line_list.append(line)
-        print(line_list)
+        print(*line_list,' ',sep="\n")
         answer += check_rolls(line_list)
-
         line_list = line_list[1:]
+        line_list.append(line)
         line = list((input.readline()).strip())
 
-    print(f"{line_list=}\n{answer=}")
+    
+    print(*line_list,' ',sep="\n")
+    line_list = line_list[1:]
+    line_list.append(buffer)
+    print(*line_list,' ',sep="\n")
+    answer += check_rolls(line_list)
 
-
+print(f"{answer=}")
