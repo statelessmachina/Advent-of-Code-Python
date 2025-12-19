dc1550b9153f28304dd108a06b9b2a37780ccf8d
Hobbes <156368504+statelessmachina@users.noreply.github.com>
2025-12-19 15:45:57 -0600

    day4/paper.py: Saving progress...
    Working on iterating through the list of lines correctly.
    
    TODO:
        - Add functionality to check roll eligibility.

--- day4/paper.py
+++ day4/paper.py
@@ -7,14 +7,16 @@ answer = 0
 with open(sys.argv[1], 'r') as input:
     # Grabs first 2 lines to start checking rolls
     # Initializes with one empty row to check top row
+    line = list((input.readline()).strip())
     lines = list()
-    while len(lines) < 3:
-        line = input.readline()
-        line = list(line.strip())
-        lines.append(line)
-    while line:
-        line = list(line.strip())
-        print(f"{len(line)=}\n{line=}")
-        
+    lines.append(list('.' * len(line)))
 
-        line = input.readline()
+    while len(lines) < 3:
+        lines.append(line)
+        line = list((input.readline()).strip())
+
+    while line:
+        print(f"{lines}")
+        lines = lines[1:]
+        lines.append(line)
+        line = list((input.readline()).strip())
