e588bcc917117278e4b8a2a145a0b6408ec78bc9
Hobbes <156368504+statelessmachina@users.noreply.github.com>
2026-01-08 11:31:52 -0600

    day6/math.py Final solution for part 1

--- 2025/day6/math.py
+++ 2025/day6/math.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3
 
 import sys
+from math import prod
+
+def collapse_list(iterable: list[int]) -> int:
+    if iterable[-1] == '*':
+        return prod(iterable[:-1])
+    else:
+        return sum(iterable[:-1])
 
 def main(input_file):
     answer = 0
@@ -8,11 +15,16 @@ def main(input_file):
         input_array = list(list(line.replace('\n','').rsplit() for line in input))
         number_array = list(list(map(int, input_array[row])) for row in range(len(input_array[:-1])))
         number_array.append(input_array[-1])
+        input_array = list(zip(*number_array))
         
-        
-        print(number_array)
-        print(list(zip(*number_array)))
+        number_array = list()
+        for line in input_array:
+            number_array.append(collapse_list(line))
+
+        number_array.append('+')
+
+        return collapse_list(number_array)
         
 
 if __name__ == "__main__":
-    main(sys.argv[1])
+    print(main(sys.argv[1]))
