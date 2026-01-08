1baa72d63d88834567ae5b22e5c66d91f911e542
Hobbes <156368504+statelessmachina@users.noreply.github.com>
2026-01-08 11:13:03 -0600

    day6/math.py Input converted to int type

--- 2025/day6/math.py
+++ 2025/day6/math.py
@@ -2,15 +2,17 @@
 
 import sys
 
-
 def main(input_file):
-
     answer = 0
     with open(input_file, 'r') as input:
         input_array = list(list(line.replace('\n','').rsplit() for line in input))
-        print(list(zip(*input_array)))
+        number_array = list(list(map(int, input_array[row])) for row in range(len(input_array[:-1])))
+        number_array.append(input_array[-1])
+        
+        
+        print(number_array)
+        print(list(zip(*number_array)))
         
 
 if __name__ == "__main__":
-
     main(sys.argv[1])
