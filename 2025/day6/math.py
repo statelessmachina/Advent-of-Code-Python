aea1d9ef5851c3e72b091c91e355c244f72cb7dd
Hobbes <156368504+statelessmachina@users.noreply.github.com>
2026-01-08 10:45:47 -0600

    day6/math.py Rewrote col grouping without numpy
    To avoid unnecessary module import, rewrote column grouping without numpy.
    I used built-ins "zip" and "list" as well as list expansion. This code was
    copied directly from the python docs.

--- 2025/day6/math.py
+++ 2025/day6/math.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
 
 import sys
-import numpy as np
 
-answer = 0
-with open(sys.argv[1], 'r') as input:
-    input_array = np.array(list(line.replace('\n','').rsplit() for line in input))
-    
-    for col in range(len(input_array[0])):
-        print(input_array[:,col])
+
+def main(input_file):
+
+    answer = 0
+    with open(input_file, 'r') as input:
+        input_array = list(list(line.replace('\n','').rsplit() for line in input))
+        print(list(zip(*input_array)))
+        
+
+if __name__ == "__main__":
+
+    main(sys.argv[1])
